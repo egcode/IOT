a7705ca370acb8f62d2ff7fd3a6bd2f7f4dc96e3
Eugene <xcode@example.com>
2023-11-18 18:35:46 -0800

    I2C example added

--- I2C/led_blink/led_blink.ino
+++ I2C/led_blink/led_blink.ino
@@ -1,10 +1,44 @@
+
+// --------------- BEGIN SIMPLE BLINK
+
+// void setup() {
+//   pinMode(LED_BUILTIN, OUTPUT);
+// }
+
+// void loop() {
+//   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+//   delay(100);                      // wait for a second
+//   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+//   delay(100);                      // wait for a second
+// }
+
+// --------------- END OF SIMPLE BLINK
+
+
+#include <Wire.h>
+
+// LED on pin 13
+const int ledPin = LED_BUILTIN; 
+
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  // Join I2C bus as slave with address 8
+  Wire.begin(0x8);
+  
+  // Call receiveEvent when data received                
+  Wire.onReceive(receiveEvent);
+  
+  // Setup pin 13 as output and turn LED off
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
 }
 
+// Function that executes whenever data is received from master
+void receiveEvent(int howMany) {
+  while (Wire.available()) { // loop through all but the last
+    char c = Wire.read(); // receive byte as a character
+    digitalWrite(ledPin, c);
+  }
+}
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(100);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(100);                      // wait for a second
+  delay(100);
 }
