d8e8fb989e4c44bd28d159e664f565e8ca4ee3e8
eugene <ea_golovanov@apple.com>
2023-12-24 15:47:35 -0800

    buttons setup1

--- I2C/face-detection/i2c_UI.py
+++ I2C/face-detection/i2c_UI.py
@@ -10,60 +10,34 @@ def on_button_click(direction):
     ########
     bus = smbus.SMBus(1)
     address = 0x8 # bus address
-
     vertical_step_num = 100
     horizontal_step_num = 300
+    num1, num2 = 0, 0
 
-    # Add conditions for each button
     if direction == "top":
-        # Code for the "Up" button
         print("Up button clicked.")
-
         num1 = 0
         num2 = int(vertical_step_num)
-        print("original num1:  " + str(num1))
-        print("original num2:  " + str(num2))
-        num1_part1, num1_part2, num1_part3, num1_part4 = bitSplitIntegerAs32Bit(num1)
-        num2_part1, num2_part2, num2_part3, num2_part4 = bitSplitIntegerAs32Bit(num2)
-        bus.write_i2c_block_data(address, num1_part1, [num1_part2, num1_part3, num1_part4, num2_part1, num2_part2, num2_part3, num2_part4])
-        time.sleep(1)
-        
     elif direction == "bottom":
-        # Code for the "Down" button
         print("Down button clicked.")
         num1 = 0
         num2 = -int(vertical_step_num)
-        print("original num1:  " + str(num1))
-        print("original num2:  " + str(num2))
-        num1_part1, num1_part2, num1_part3, num1_part4 = bitSplitIntegerAs32Bit(num1)
-        num2_part1, num2_part2, num2_part3, num2_part4 = bitSplitIntegerAs32Bit(num2)
-        bus.write_i2c_block_data(address, num1_part1, [num1_part2, num1_part3, num1_part4, num2_part1, num2_part2, num2_part3, num2_part4])
-        time.sleep(1)
-
     elif direction == "left":
-        # Code for the "Left" button
         print("Left button clicked.")
         num1 = int(horizontal_step_num)
         num2 = 0
-        print("original num1:  " + str(num1))
-        print("original num2:  " + str(num2))
-        num1_part1, num1_part2, num1_part3, num1_part4 = bitSplitIntegerAs32Bit(num1)
-        num2_part1, num2_part2, num2_part3, num2_part4 = bitSplitIntegerAs32Bit(num2)
-        bus.write_i2c_block_data(address, num1_part1, [num1_part2, num1_part3, num1_part4, num2_part1, num2_part2, num2_part3, num2_part4])
-        time.sleep(1)
-
     elif direction == "right":
-        # Code for the "Right" button
         print("Right button clicked.")
         num1 = -int(horizontal_step_num)
         num2 = 0
-        print("original num1:  " + str(num1))
-        print("original num2:  " + str(num2))
-        num1_part1, num1_part2, num1_part3, num1_part4 = bitSplitIntegerAs32Bit(num1)
-        num2_part1, num2_part2, num2_part3, num2_part4 = bitSplitIntegerAs32Bit(num2)
-        bus.write_i2c_block_data(address, num1_part1, [num1_part2, num1_part3, num1_part4, num2_part1, num2_part2, num2_part3, num2_part4])
-        time.sleep(1)
 
+    print("original num1:  " + str(num1))
+    print("original num2:  " + str(num2))
+    num1_part1, num1_part2, num1_part3, num1_part4 = bitSplitIntegerAs32Bit(num1)
+    num2_part1, num2_part2, num2_part3, num2_part4 = bitSplitIntegerAs32Bit(num2)
+    bus.write_i2c_block_data(address, num1_part1, [num1_part2, num1_part3, num1_part4, num2_part1, num2_part2, num2_part3, num2_part4])
+    time.sleep(1)
+        
 # Create the main window
 root = tk.Tk()
 root.title("4-Sided Controller")
